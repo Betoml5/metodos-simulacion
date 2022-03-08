9813ae9412d5f7d877c5b7fd19244fa88924cd0f
Betoml5 <betoml5@hotmail.com>
2022-03-07 21:55:49 -0600

    Finish methods

---
+++ metodoMixto.py
@@ -0,0 +1,17 @@
+
+
+from re import A
+from unittest import result
+
+
+a = 50
+semilla = 15
+c = 16
+moduloInicial = 100
+
+resultado = (a * semilla + c) % moduloInicial
+
+numeroAleatorio = resultado / (moduloInicial-1)
+
+print(resultado)
+print(numeroAleatorio)
